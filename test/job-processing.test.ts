9b0e7b116ba6d52d5a6ae402b550b1d5787c6800
SamaMansour <sma302000@gmail.com>
2026-02-17 22:43:05 +0300

    Initial commit

---
+++ test/job-processing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JobProcessing from '../lib/job-processing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/job-processing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JobProcessing.JobProcessingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
